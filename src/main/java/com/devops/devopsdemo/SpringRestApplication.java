e700f1baf517b289320dc51f3fc386ae3858516e
sovisrushain <sovisrushain@gmail.com>
2023-05-21 16:05:10 +0530

    fix bugs

--- src/main/java/com/devops/devopsdemo/SpringRestApplication.java
+++ src/main/java/com/devops/devopsdemo/SpringRestApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DevopsdemoApplication {
+public class SpringRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevopsdemoApplication.class, args);
+		SpringApplication.run(SpringRestApplication.class, args);
 	}
 
 }
