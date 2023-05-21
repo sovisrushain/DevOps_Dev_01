eb0ef7f9bdfe94f5dae1a6776cbb5e66c2c6c21a
sovisrushain <sovisrushain@gmail.com>
2023-05-21 15:38:49 +0530

    fix bugs

--- src/main/java/com/devops/devopsdemo/DevopsdemoApplication.java
+++ src/main/java/com/devops/devopsdemo/DevopsdemoApplication.java
@@ -2,8 +2,10 @@ package com.devops.devopsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DevopsdemoApplication {
 
 	public static void main(String[] args) {
